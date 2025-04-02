064747cc88446b99e99118e402a529118fbf63f8
ACER <ACER@DESKTOP-L27N6PR>
2025-04-03 00:07:25 +0700

    Update code

--- admincp/index.php
+++ admincp/index.php
@@ -4,14 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>adminncp</title>
+    <link rel="stylesheet" type="text/css" href="css/styleadmincp.css">
 </head>
-<body>
-    <h3>wellcome to abcdsf</h3>
+<div>
+    <h3 class="title_admin">wellcome to AdminCP</h3>
+    <div class="wrapper">
     <?php 
+        include("config/config.php");
         include("modules/header.php");
         include("modules/menu.php");
         include("modules/main.php");
         include("modules/footer.php");
     ?>
+    </div>
 </body>
 </html>
